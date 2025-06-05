57036e7957f36468a554f3c4c03634c069465845
Mahmudul Islam <mahmudulislammern@gmail.com>
2025-06-05 19:25:32 +0600

    4.21

--- src/App.jsx
+++ src/App.jsx
@@ -1,60 +1,11 @@
-import { useState } from "react";
-import ChatRoom from "./components/ChatRoom";
-import ChatServerRoom from "./components/ChatServerRoom";
-import { SettingsContext } from "./context/SettingsContext";
+import Timer from "./components/4.21/Counter.jsx";
 
-export default function App() {
-  const [roomId, setRoomId] = useState("general");
-  const [showChat, setShowChat] = useState(true);
-  const [serverUrl, setserverUrl] = useState(null);
-
-  const handleRoomChange = (e) => {
-    setRoomId(e.target.value);
-  };
-  const handleServerChange = (e) => {
-    setserverUrl(e.target.value);
-  };
-
+const App = () => {
   return (
     <div>
-      {/* <div>
-        <input
-          type="text"
-          value={serverUrl}
-          onChange={(e) => setServerUrl(e.target.value)}
-        />
-      </div> */}
-      <div>
-        Select Server:
-        <select onChange={handleServerChange}>
-          <option value="http://localhost:1234">Server 1</option>
-          <option value="http://localhost:1235">Server 2</option>
-          <option value="http://localhost:1236">Server 3</option>
-        </select>
-      </div>
-      <div>
-        <button onClick={() => setShowChat((s) => !s)}>
-          {showChat ? "Hide Chat Room" : "Show Chat Room"}
-        </button>
-      </div>
-      {showChat && (
-        <>
-          <hr />
-          <div>
-            Select Server:
-            <select onChange={handleRoomChange}>
-              <option value="general">General</option>
-              <option value="travel">Travel</option>
-              <option value="music">Music</option>
-            </select>
-          </div>
-          <SettingsContext.Provider
-            value={{ defaultServerUrl: "http://localhost:1111" }}
-          >
-            <ChatServerRoom roomId={roomId} selectedServerURL={serverUrl} />
-          </SettingsContext.Provider>
-        </>
-      )}
+      <Timer />
     </div>
   );
-}
+};
+
+export default App;
---
+++ src/components/4.21/Counter.jsx
@@ -0,0 +1,49 @@
+import {
+  useState,
+  useEffect,
+  experimental_useEffectEvent as useEffectEvent,
+} from "react";
+
+export default function Timer() {
+  const [count, setCount] = useState(0);
+  const [increment, setIncrement] = useState(1);
+
+  const onTick = useEffectEvent(() => {
+    setCount(count + increment);
+  });
+
+  useEffect(() => {
+    const timerid = setInterval(onTick, 1000);
+    // clean up
+    return () => clearInterval(timerid);
+  }, []);
+
+  return (
+    <>
+      <h1>
+        Counter: {count}
+        <button onClick={() => setCount(0)}>Reset</button>
+      </h1>
+      <hr />
+      <p>
+        Every second, increment by:
+        <button
+          disabled={increment === 0}
+          onClick={() => {
+            setIncrement((i) => i - 1);
+          }}
+        >
+          –
+        </button>
+        <b>{increment}</b>
+        <button
+          onClick={() => {
+            setIncrement((i) => i + 1);
+          }}
+        >
+          +
+        </button>
+      </p>
+    </>
+  );
+}
